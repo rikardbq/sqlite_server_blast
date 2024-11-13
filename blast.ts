fd9e1be80af7719f85224a5f47e8af673e52d860
rikardbq <j10rikardb@gmail.com>
2024-11-13 18:59:03 +0100

    small updates

--- blast.ts
+++ blast.ts
@@ -1,19 +1,22 @@
 // Blastoise
 const args = Deno.args;
-console.log("args", args[0]);
+
+const endpoint = "http://localhost:8080/testing";
+const body = {
+    method: "POST",
+    body: JSON.stringify({
+        payload: args[0],
+    }),
+    headers: {
+        "content-type": "application/json",
+        u_: "b1a74559bea16b1521205f95f07a25ea2f09f49eb4e265fa6057036d1dff7c22",
+    },
+};
+
+console.table({ endpoint, ...body });
 
 export async function handler() {
-    const request = new Request("http://localhost:8080/testing", {
-        method: "POST",
-        body: JSON.stringify({
-            payload:
-                args[0] ?? "some_token",
-        }),
-        headers: {
-            "content-type": "application/json",
-            u_: "b1a74559bea16b1521205f95f07a25ea2f09f49eb4e265fa6057036d1dff7c22"
-        },
-    });
+    const request = new Request(endpoint, body);
 
     await fetch(request);
 }
